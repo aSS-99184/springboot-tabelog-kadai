29312e4d159963379cff8348cfb03929abf505c3
GitHubのユーザー名 <GitHub>
2025-04-19 16:24:34 +0900

    新規作成

---
+++ src/main/java/com/example/samuraitabelog/SamuraitabelogApplication.java
@@ -0,0 +1,13 @@
+package com.example.samuraitabelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuraitabelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuraitabelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samuraitabelog/SamuraitabelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samuraitabelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamuraitabelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
